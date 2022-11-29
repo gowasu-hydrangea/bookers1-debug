70c8225473f9b3c4b5f30ac5f03f754d56e10aa5
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-11-29 08:31:30 +0000

    [Finish]1129

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    # @book_id = Book.find(params[:id])
   end
 
   def create
@@ -27,7 +28,8 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
+
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
